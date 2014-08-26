ea54d32da56e323eb3857808f1e8ae1aa740569f
Audun Halland <audun.halland@gmail.com>
2014-08-26 13:23:22 +0200

    iOS - empty app

---
+++ ios/poapp/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  poapp
+//
+//  Created by Audun Halland on 26/08/14.
+//  Copyright (c) 2014 Audun Halland. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
